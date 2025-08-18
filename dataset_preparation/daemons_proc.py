48d25bdfe62188e0e1991e98457346b789278bc3
Fabio Bucher <fabio.bucher@uzh.ch>
2025-08-18 16:49:23 +0200

    fixed bugs in daemons_proc data preprocessing script, added documentation

--- dataset_preparation/daemons_proc.py
+++ dataset_preparation/daemons_proc.py
@@ -7,31 +7,41 @@ import pandas as pd
 from math import pi, tan
 
 def download_data(url: str, extract: list[str], zip_path: str):
+    """This downloads a zip file from a url, extracts all files in extract and writes them to a 'datasets' subfolder.
+    The download is skipped if a zip at zip_path already exists."""
     if not os.path.exists(zip_path):
+        print(f"Downloading data from: {url}\n...")
         movements = requests.get(url).content
         with open(zip_path, "wb") as f:
             f.write(movements)
+    print(f"Extracting into 'datasets' folder: \n{extract}")
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall("datasets", extract)
 
 def create_subset_of_df():
-    csv_path = "datasets/DAEMONS/fixations.csv"
+    """Creates subset of dataset, with only subjects with ids 1-10 and moves it together with the readme to a subfolder 'datasets/DAEMONS'."""
+    csv_path = "datasets/eye_movement/SAC_val.csv"
     df = pd.read_csv(csv_path, index_col=0)
     subset_of_ids_df = df[df["VP"] < 10]
     subset_df = subset_of_ids_df[subset_of_ids_df["Img"].str.contains("DAEMONS_corpus_potsdam")]
     subset_images = ["DAEMONS_potsdam_corpus"+"/"+path for path in set(subset_df["Img"])]
-    subset_df["image_name"] = subset_df["Img"].apply(lambda x: x[-8:])
+    image_name_series = subset_df["Img"].apply(lambda x: x[-8:])
+    subset_df.loc[:,"image_name"] = image_name_series
     subset_df = subset_df.drop(columns=["Img"])
-    subset_df = subset_df.rename({"VP": "subject_id", "fixdur": "fixation_duration"})
+    subset_df = subset_df.rename(columns={"VP": "subject_id", "fixdur": "fixation_duration"})
     subset_df.to_csv(csv_path)
+    print("Writing subset of data to 'datasets/DAEMONS'\n...")
+    os.rename(csv_path, "datasets/DAEMONS/fixations.csv")
+    os.rename("datasets/eye_movement/readme.md", "datasets/DAEMONS/readme.md")
     return subset_images
 
 def add_xy_px():
+    """Converts x and y row in original df (which are in degree of visual angle) to screen space coordinates replacing the original data columns."""
     csv_path = "datasets/DAEMONS/fixations.csv"
     df = pd.read_csv(csv_path, index_col=0)
     df[["x_px", "y_px"]] = df.apply(visual_angle_to_px, axis=1)
-    df.drop(["x", "y"])
-    df.rename({"x_px": "x", "y_px": "y"})
+    df = df.drop(columns=["x", "y"])
+    df = df.rename(columns={"x_px": "x", "y_px": "y"})
     df.to_csv(csv_path)
 
 def visual_angle_to_px(row) -> pd.Series:
@@ -50,24 +60,33 @@ def visual_angle_to_px(row) -> pd.Series:
     return pd.Series([x_px, y_px], index=['x_px', 'y_px'])
 
 def rename_images():
+    """Removes the 'DAEMONS_potsdam_corpus' prefix from image filenames."""
     corpus_path = "datasets/DAEMONS_potsdam_corpus"
     for path in os.listdir(corpus_path):
         if path.startswith("DAEMONS_corpus_potsdam"):
             abs_path = corpus_path + "/" + path
-            new_path = corpus_path + "/" + path[-8:]
+            new_path = "datasets/DAEMONS" + "/" + "img" + "/" + path[-8:]
             os.rename(abs_path, new_path)
+    
 
 if __name__ == "__main__":
+    # create directory structure for resulting dataset:
+    if not os.path.exists("datasets/DAEMONS"):
+        os.makedirs("datasets/DAEMONS/img")
     # download movements
     download_data("https://osf.io/download/ztgna/", 
                   ["eye_movement/SAC_val.csv", "eye_movement/readme.md"],
                   "datasets/daemons.zip")
     subset_images = create_subset_of_df()
-    # download images
+    # download subset of images
     download_data("https://osf.io/download/x5jb2/", 
                   subset_images,
                   "datasets/images.zip")
+    # convert degrees of visual angle to screen coordinates
     add_xy_px()
     rename_images()
+    # remove emtpy DAEMONS_potsdam_corpus and eye_movement directories
+    os.rmdir("datasets/DAEMONS_potsdam_corpus")
+    os.rmdir("datasets/eye_movement")
     
     
